0b26807a5a904239b2a3bf33b7ca15b6d0fe6799
adersonjunior85 <46459925+adersonjunior85@users.noreply.github.com>
2021-06-22 16:33:01 -0300

    a
    a

--- executeRaspador.py
+++ executeRaspador.py
@@ -4,44 +4,34 @@ def rasparTodosJogos(esporte):
     driver = raspador.iniciaNavegador()
     dataRasparTodosJogos = raspador.raspadorPaises(driver,esporte)
     raspador.fechaNavegador(driver)
-    print(dataRasparTodosJogos)
     return dataRasparTodosJogos
 
 def rasparLigas(esporte,liga):
     driver = raspador.iniciaNavegador()
     dataLiga = raspador.raspadorLigasPaises(driver,esporte,liga)
     raspador.fechaNavegador(driver)
-    print(dataLiga)
     return dataLiga
 
 def lerTodasOddsPorLiga(esporte,liga):
     driver = raspador.iniciaNavegador()
     dataLiga = raspador.raspadorLigasPaises(driver,esporte,liga)
     oddsLiga = []
-    print("dataLiga")
     for x in dataLiga[liga]:
         _id = x.get("id")
         dataOdds = raspador.capturaOddLink(driver,_id)
         oddsLiga.append({x.get('Equipes'):dataOdds})
     dataExport = {liga:oddsLiga}
-    print(dataExport)
+    raspador.fechaNavegador(driver)
     return dataExport
 
 def lerAovivo():
     driver = raspador.iniciaNavegador()
     jogosAovivo = raspador.aoVivo(driver)
-    print(jogosAovivo)
+    raspador.fechaNavegador(driver)
     return jogosAovivo
 
 def capturaEsportes():
     driver = raspador.iniciaNavegador()
     esportes = raspador.capturaEsportesPaginaInicial(driver)
-    print(esportes)
+    raspador.fechaNavegador(driver)
     return esportes
-
-    
-#rasparLigas("tennis","Nottingham")
-#lerTodasOddsPorLiga("tennis","Nottingham")
-#data = rasparTodosJogos("soccer")
-#lerAovivo()
-#capturaEsportes()
