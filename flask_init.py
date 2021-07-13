0acf0afb5567f9821a914b907732b86e45d87905
adersonjunior85 <46459925+adersonjunior85@users.noreply.github.com>
2021-07-13 15:20:06 -0300

    a
    a

--- flask_init.py
+++ flask_init.py
@@ -49,4 +49,4 @@ def callFunction_lerAovivo():
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True, ssl_context='adhoc')
+	app.run(host='0.0.0.0', port=port, debug=True)
